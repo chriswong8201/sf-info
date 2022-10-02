cac759455c069659710d64f43b61acf533a029e7
chriswong8021 <chriswong8201@gmail.com>
2022-10-02 14:41:19 +0800

    testing6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          SFVI info page
+          SFVI info page2
         </a>
       </header>
     </div>
